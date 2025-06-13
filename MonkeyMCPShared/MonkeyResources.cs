d509fabbbbd89618782588235668c3e033cfcabb
James Montemagno <james.montemagno@gmail.com>
2025-06-13 09:46:15 -0700

    Add Baboon resource method and implement GitHub Actions workflow for build and publish

--- MonkeyMCPShared/MonkeyResources.cs
+++ MonkeyMCPShared/MonkeyResources.cs
@@ -15,8 +15,16 @@ public class MonkeyResources
         this.monkeyService = monkeyService;
     }
 
+    [McpServerResource(UriTemplate = "monkeymcp://monkeys/baboon", Name = "Baboon", MimeType = "application/json")]
+    [Description("Get details of a baboon monkey")]
+    public async Task<string> Baboon()
+    {
+        var baboon = await monkeyService.GetMonkey("Baboon") ?? throw new Exception($"Baboon not found");
+        return JsonSerializer.Serialize(baboon, MonkeyContext.Default.Monkey);
+    }
 
-    [McpServerResource, Description("Get monkey details by name")]
+    [McpServerResource(UriTemplate = "monkeymcp://monkeys/{name}", Name = "Monkey")]
+    [Description("Get monkey details by name")]
     public async Task<string> Monkey(string name)
     {
         var monkey = await monkeyService.GetMonkey(name) ?? throw new Exception($"{name} not found");
